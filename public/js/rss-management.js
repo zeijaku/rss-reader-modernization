51d1c22e476b3934df232bf10d34c1695b4bf0f4
zeijaku <zeijaku.net@gmail.com>
2026-09-03 00:22:22 +0900

    fix(v1.30): align RSS Rules asset cache keys

--- public/js/rss-management.js
+++ public/js/rss-management.js
@@ -179,8 +179,8 @@
         });
     });
 
-    $.getScript('./js/rss-rules.js?v=1.29.0').done(function () {
-        $.getScript('./js/rss-rules-integration.js?v=1.29.0');
+    $.getScript('./js/rss-rules.js?v=1.30.0').done(function () {
+        $.getScript('./js/rss-rules-integration.js?v=1.30.0');
     });
     $(loadFeeds);
 })(jQuery, document, window);
