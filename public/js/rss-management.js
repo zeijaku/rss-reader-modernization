fa3fcc8b97f2cf581807014edb8071d3efe8e731
zeijaku <zeijaku.net@gmail.com>
2026-09-12 17:03:04 +0900

    V1.34.2-B: synchronize RSS rule cache to dev.3

--- public/js/rss-management.js
+++ public/js/rss-management.js
@@ -179,8 +179,8 @@
         });
     });
 
-    $.getScript('./js/rss-rules.js?v=1.34.2-dev.2').done(function () {
-        $.getScript('./js/rss-rules-integration.js?v=1.34.2-dev.2');
+    $.getScript('./js/rss-rules.js?v=1.34.2-dev.3').done(function () {
+        $.getScript('./js/rss-rules-integration.js?v=1.34.2-dev.3');
     });
     $(loadFeeds);
 })(jQuery, document, window);
