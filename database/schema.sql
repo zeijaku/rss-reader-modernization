55fb476ed0cae5f1d46094659fb24f831571b06c
zeijaku <zeijaku.net@gmail.com>
2026-09-25 23:57:11 +0900

    V1.36-A integrate notification fresh-install schema

--- database/schema.sql
+++ database/schema.sql
@@ -22,6 +22,7 @@ SET @t_task = CONCAT('`', @table_prefix, 'task`');
 SET @t_calendar_event = CONCAT('`', @table_prefix, 'calendar_event`');
 SET @t_calendar_event_exception = CONCAT('`', @table_prefix, 'calendar_event_exception`');
 SET @t_dashboard_widget = CONCAT('`', @table_prefix, 'dashboard_widget`');
+SET @t_notification = CONCAT('`', @table_prefix, 'notification`');
 SET @t_remember_token = CONCAT('`', @table_prefix, 'remember_token`');
 SET @t_user_file = CONCAT('`', @table_prefix, 'user_file`');
 SET @t_remote_connection = CONCAT('`', @table_prefix, 'remote_connection`');
@@ -209,6 +210,31 @@ SET @sql = CONCAT(
   ') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT=''Calendar occurrence override and cancellation'''
 );
 PREPARE v133d_stmt FROM @sql; EXECUTE v133d_stmt; DEALLOCATE PREPARE v133d_stmt;
+
+-- V1.36-A Dashboard Notification Center.
+SET @sql = CONCAT(
+  'CREATE TABLE ', @t_notification, ' (',
+  '`notification_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,',
+  '`notification_owner` INT UNSIGNED NOT NULL COMMENT ''user_info.user_id'',',
+  '`notification_type` VARCHAR(16) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,',
+  '`notification_source_type` VARCHAR(16) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,',
+  '`notification_source_id` VARCHAR(64) NULL DEFAULT NULL,',
+  '`notification_source_key` VARCHAR(191) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,',
+  '`notification_title` VARCHAR(256) NOT NULL,',
+  '`notification_body` VARCHAR(1024) NOT NULL DEFAULT '''',',
+  '`notification_target_url` VARCHAR(2048) NULL DEFAULT NULL,',
+  '`notification_due_at` DATETIME NOT NULL,',
+  '`notification_read_at` DATETIME NULL DEFAULT NULL,',
+  '`notification_hidden_at` DATETIME NULL DEFAULT NULL,',
+  '`notification_created_at` DATETIME NOT NULL,',
+  '`notification_updated_at` DATETIME NOT NULL,',
+  'PRIMARY KEY (`notification_id`),',
+  'UNIQUE KEY `uq_notification_owner_source` (`notification_owner`, `notification_source_type`, `notification_source_key`, `notification_type`),',
+  'KEY `idx_notification_owner_visible_due` (`notification_owner`, `notification_hidden_at`, `notification_due_at`, `notification_id`),',
+  'KEY `idx_notification_owner_unread_due` (`notification_owner`, `notification_read_at`, `notification_hidden_at`, `notification_due_at`)',
+  ') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT=''Dashboard notifications'''
+);
+PREPARE v136a_schema_stmt FROM @sql; EXECUTE v136a_schema_stmt; DEALLOCATE PREPARE v136a_schema_stmt;
 
 SET @sql = CONCAT(
   'CREATE TABLE ', @t_task, ' (',
